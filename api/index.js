5d0e87fdf917af60ed5031310ea1142ac8be2881
prashantmathias <mathiasaudio@gmail.com>
2016-11-11 08:07:06 +0000

    adds middleware and connection to mongo db

--- api/index.js
+++ api/index.js
@@ -1,1 +1,20 @@
+var express = require('express');
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
 
+var app = express();
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+app.use('/test',function(req, res, next) {
+  res.send('Hello from preset-loader-api');
+  next();
+});
+
+mongoose.connect('mongodb://localhost/preset-loader-app');
+mongoose.connection.once('open', function() {
+
+  console.log('Listening on port 3000');
+  app.listen(3000);
+});
